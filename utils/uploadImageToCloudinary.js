4f59d80304f083b7df34658560baba71dd21e7aa
Ángel Pereira <ampr2003@gmail.com>
2025-01-08 09:44:46 +0100

    Agregar configuración y funciones para subir imágenes a Cloudinary, incluyendo soporte en la creación y actualización de usuarios.

---
+++ utils/uploadImageToCloudinary.js
@@ -0,0 +1,19 @@
+import { cloudinary } from "./cloudinaryConfig.js";
+
+export async function uploadImage(imagePath) {
+
+  const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+    public_id: imagePath,
+    folder: "users"
+  };
+
+  try {
+    const result = await cloudinary.uploader.upload(imagePath, options);
+    return result.secure_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
