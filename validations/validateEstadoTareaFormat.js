5aa14c06863ea943c90bba8eba889f3e68be4dc7
avilrod3004 <avilrod3004@g.educaand.es>
2025-01-10 10:21:19 +0100

    middleware para validar el formato del estado de las tareas

---
+++ validations/validateEstadoTareaFormat.js
@@ -0,0 +1,13 @@
+import {BadRequestError} from "../errors/BadRequestError.js";
+
+export const validateEstadoTareaFormat = (paramName = 'prioridad') => {
+    return (req, res, next) => {
+        const paramValue = req.params[paramName] || '';
+
+        if (paramValue === "pendiente" || paramValue === "en_proceso" || paramValue === "en_revision" || paramValue === "completada") {
+            next();
+        } else {
+            next(new BadRequestError(`param_${paramName}_is_not_valid_state`.toLowerCase()));
+        }
+    }
+};
