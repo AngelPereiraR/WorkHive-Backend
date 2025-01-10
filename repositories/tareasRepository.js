f984cd12af491cc8dd66010f5db1403f8ed7b7da
avilrod3004 <avilrod3004@g.educaand.es>
2025-01-10 10:11:49 +0100

    correccion del repositorio de las tareas

--- repositories/tareasRepository.js
+++ repositories/tareasRepository.js
@@ -5,25 +5,33 @@ async function create(data) {
 }
 
 async function list() {
-    return await TareaModel.find().sort({nombre: "DESC"}).exec();
+    return await TareaModel.find().sort({ nombre: "DESC" }).exec();
 }
 
-async function listByPriority(priority) {
-    return await TareaModel.find({ _prioridad: priority }).exec();
+async function listByPriority(tablero, priority) {
+    return await TareaModel.find({ tablero: tablero, prioridad: priority }).exec();
 }
 
-async function listByState(state) {
-    return await TareaModel.find( { _estado: state }).exec()
+async function listByState(tablero, state) {
+    return await TareaModel.find( { tablero: tablero, estado: state }).exec()
 }
 
-async function listByLimitDate(date) {
-    return await TareaModel.find({ _fechaLimite: date }).exec();
+async function listByLimitDate(tablero, date) {
+    return await TareaModel.find({ tablero: tablero, fechaLimite: date }).exec();
 }
 
-async function listByUserAsigned(id) {
-    return await TareaModel.find( { _asignadoA: id} ).exec();
+async function listByUserAsigned(tablero, id) {
+    return await TareaModel.find( { tablero: tablero, asignadoA: id} ).exec();
 }
 
+/**
+ * Obtiene una tarea por su ID.
+ *
+ * @async
+ * @function getOne
+ * @param {string} id - ID único de la tarea.
+ * @returns {Promise<Object|null>} La tarea encontrada o null si no existe.
+ */
 async function getOne(id){
     const params = { _id: id };
     return await TareaModel.findOne(params).exec();
@@ -33,6 +41,15 @@ async function remove(id) {
     return await TareaModel.findOneAndDelete({ _id: id }).exec();
 }
 
+/**
+ * Actualiza la tarea por su ID con los datos proporcionados.
+ *
+ * @async
+ * @function update
+ * @param {string} id - ID único de la tarea a actualizar.
+ * @param {Object} data - Datos de la tarea a actualizar.
+ * @returns {Promise<Object|null>} La tarea actualizada o null si no existe.
+ */
 async function update(id, data) {
     return await TareaModel.findOneAndUpdate({ _id: id}, data, { new: true, runValidators: true }).exec();
 }
